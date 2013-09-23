f1d14e9e984957163acf32cfb8545603310401c8
Matthew Shafer <mshafer1@harding.edu>
2013-09-23 17:15:39 -0500

    Created
    Need to add file output for basic login/logout

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IDField = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // IDField
+            // 
+            this.IDField.Location = new System.Drawing.Point(11, 15);
+            this.IDField.Name = "IDField";
+            this.IDField.Size = new System.Drawing.Size(201, 20);
+            this.IDField.TabIndex = 0;
+            this.IDField.TextChanged += new System.EventHandler(this.ID_TextChanged);
+            // 
+            // OK
+            // 
+            this.OK.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.OK.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.OK.Location = new System.Drawing.Point(230, 13);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(45, 21);
+            this.OK.TabIndex = 1;
+            this.OK.Text = "OK";
+            this.OK.UseCompatibleTextRendering = true;
+            this.OK.UseVisualStyleBackColor = true;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            this.OK.ControlAdded += new System.Windows.Forms.ControlEventHandler(this.Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.IDField);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IDField;
+        private System.Windows.Forms.Button OK;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            if (File.Exists(FILE_NAME))
+            {
+                FileStream fsIn = new FileStream(FILE_NAME, FileMode.Open,
+                FileAccess.Read, FileShare.Read);
+                users = new SelfBalancedTree.AVLTree<User>();
+                using (StreamReader sr = new StreamReader(fsIn))
+                {
+                    string input;
+                    // While not at the end of the file, read lines from the file. 
+                    while (sr.Peek() > -1)
+                    {
+                        input = sr.ReadLine();
+                        User newUser = new User();
+                        newUser.setName(ref input);
+                        input = sr.ReadLine();
+                        newUser.setId(input);
+                        users.Add(newUser);
+                    }
+                }
+            }
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            
+            
+            User current = new User();
+            DateTime currentTime = new DateTime();
+            currentTime = DateTime.Now;
+            //currentTime.ToLocalTime();
+            current.setId(id);
+            if (users.Contains(current))
+            {
+                User temp = users.publicSearch(current);
+                    string name = "";
+                    temp.getName(ref name);
+                    current.setName(ref name);
+            }
+            
+            IDField.Text = "";
+            IDField.Update();
+            if (!currentlyIn.Contains(current) && users.Contains(current))
+            {
+                current.setTimeIn(currentTime);
+                currentlyIn.Add(current);
+            }
+            else if (currentlyIn.Contains(current))
+            {
+                current = (User)currentlyIn.publicSearch(current);
+                currentlyIn.Delete(current);
+                current.setTimeOut(currentTime);
+                //store log
+            }
+            else
+            {
+                MessageBox.Show("Please either retry or see system administrator for help", "User not recognized.", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk);
+            }
+        }
+
+        private void ID_TextChanged(object sender, EventArgs e)
+        {
+             IDField.PasswordChar = '*';
+             id = IDField.Text;
+        }
+
+        private DateTime time;
+        public string id;
+        private SelfBalancedTree.AVLTree<User> currentlyIn = new SelfBalancedTree.AVLTree<User>();
+        private SelfBalancedTree.AVLTree<User> users;
+        private const string FILE_NAME = "Users.dat";
+
+        
+
+        private void Enter(object sender, ControlEventArgs e)
+        {
+            OK.Select();
+        }
+
+    }
+
+
+    class User: IComparable<User>
+    {
+        public void getName(ref String nameGet)
+        {
+            nameGet = name;
+        }
+        public void setName(ref String newName)
+        {
+            name = newName;
+        }
+        public void setId(String newId)
+        {
+           ID = newId;
+        }
+        public void setTimeIn(DateTime newTimeIn)
+        {
+            timeIn = newTimeIn;
+        }
+        public void setTimeOut(DateTime newTimeOut)
+        {
+            timeOut = newTimeOut;
+        }
+        public void getTime(ref long time)
+        {
+            time = timeOut.Ticks - timeIn.Ticks;
+        }
+        public void getID(String outputID)
+        {
+            outputID = ID;
+        }
+        private string name;
+        private string ID;
+        private DateTime timeIn, timeOut;
+       public int CompareTo(User input)
+        {
+            int result;
+            if (this.ID == input.ID)
+            {
+                result = 0;
+            }
+            else result = String.Compare(this.ID, input.ID);
+            return result;
+
+        
+        }
+    }
+}
